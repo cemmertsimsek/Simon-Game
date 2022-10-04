8c87c15ab2ba10d6b5b591f414367d56a0ad3951
cem <cemsimseksm@gmail.com>
2022-10-04 17:50:24 +0300

    project finished

--- game.js
+++ game.js
@@ -1,7 +1,19 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var started = false;
+var level = 0;
+
 var gamePattern = [];
 var userClickedPattern = [];
+
+$(document).keypress(function () {
+  if (!started) {
+    $("#level-title").text("Level" + level);
+    nextSequence();
+  }
+});
+
+//when user clicks button, this function uses other functions to play sound and activating flash animation. then checks the answer
 
 $(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
@@ -9,10 +21,40 @@ $(".btn").click(function () {
 
   playSound(userChosenColour);
 
-  animatePress(userChosenColor);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+// ------------------------------
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    // console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    // console.log("wrong");
+    playSound("wrong");
+    //adding red background for 200ms
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    //changing the title from the level count to the Game Over text
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -23,14 +65,23 @@ function nextSequence() {
   playSound(randomChosenColour);
 }
 
+//function for playing audio. founds the given names pattern and plays the audio.
 function playSound(name) {
   var audio = new Audio("sounds/" + name + "mp3");
   audio.play();
 }
 
+// function for flash animation. adds pressed class for 100ms then removes it ------
+
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
-  setTimeOut(function () {
+  setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
